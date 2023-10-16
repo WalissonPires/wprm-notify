02f1f27909fcbbe2618b6b6489b70c289f526b6a
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2023-10-16 11:06:17 -0300

    Change new notification trigger page path

--- src/common/routes/index.tsx
+++ src/common/routes/index.tsx
@@ -2,7 +2,7 @@
 
 export abstract class AppRoutes {
 
-  public static newContactNotification(contactId: string) { return `/contact-notifications/${contactId}/new`; }
+  public static newContactNotification(contactId: string) { return `/contacts/${contactId}/notification-triggers/new`; }
   public static contactNotificationTriggers(contactId: string) { return `/contacts/${contactId}/notification-triggers`; }
   public static contactNotifications(contactId: string) { return `/contacts/${contactId}/notifications`; }
   public static home() { return '/'; }
