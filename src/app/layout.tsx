7cb90f0cc9c4f177639ef3396b4bbaa0314b8b82
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2023-10-16 12:16:40 -0300

    Setup PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,22 @@ export default function RootLayout({ children }: {
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="application-name" content="Wprm Notify" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="Wprm Notify" />
+        <meta name="description" content="App de envio de notificações" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="msapplication-TileColor" content="#22c55e" />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content="#22c55e" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      </head>
       <body className={inter.className}>
           {children}
       </body>
