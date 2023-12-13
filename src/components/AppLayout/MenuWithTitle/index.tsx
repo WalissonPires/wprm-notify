cd55aaade4cbc962f1ef930a3b5fe77b6a8de350
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2023-12-13 20:19:52 -0300

    Update logo

--- src/components/AppLayout/MenuWithTitle/index.tsx
+++ src/components/AppLayout/MenuWithTitle/index.tsx
@@ -1,13 +1,11 @@
-'use client'
-
 import { ReactNode } from "react";
-import { Bars3Icon } from "@heroicons/react/24/outline";
+import Image from "next/image";
 
 export function AppLayoutMenuWithTitle({ title }: AppLayoutMenuWithTitleProps) {
 
   return (
     <div className="flex items-center">
-      <span onClick={() => alert('clicked')}><Bars3Icon className="h-5 w-5 mr-4 cursor-pointer hover:scale-110" title="Menu" /></span>
+      <Image src="/favicon-light-32x32.png" alt="Logo" height="25" width="25" className="mr-2" />
       <h1 className="text-base inline-block">{title ?? 'Wprm Notify'}</h1>
     </div>
   );
