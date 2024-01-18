767caac99c66c6917333b4c0555d1ad2e887bd6d
Walisson Pires R. Miranda <walissonpiresgv@hotmail.com>
2024-01-17 23:01:11 -0300

    Apply style input disabled

--- src/components/Form/Input/index.tsx
+++ src/components/Form/Input/index.tsx
@@ -4,7 +4,7 @@ import { forwardRef } from "react";
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ className, ...props }, ref) {
 
-  return <input ref={ref} className={`appearance-none bg-transparent w-full border-b border-teal-400 hover:border-teal-500 text-gray-700 py-1 px-2 leading-tight focus:outline-none ${className ?? ''}`} {...props} />;
+  return <input ref={ref} className={`appearance-none bg-transparent w-full border-b border-teal-400 hover:border-teal-500 text-gray-700 py-1 px-2 leading-tight focus:outline-none disabled:bg-slate-200 disabled:border-slate-300 disabled:text-slate-400 ${className ?? ''}`} {...props} />;
 });
 
 export interface InputProps extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> {
